99c7f11e2328535c9c456699a86807c950e57566
Mavezo <mavezom@gmail.com>
2024-02-22 19:59:40 +0100

    #

--- GamesLibrary/Data/Game.cs
+++ GamesLibrary/Data/Game.cs
@@ -10,21 +10,21 @@ namespace GamesLibrary.Data
 		public string Description { get; set; } = default!;
 		public int Price { get; set; }
 		public string PosterImagePath { get; set; } = default!;
-		public List<VideoLinks> VideoLinks { get; set; } = default!;
-		public List<ScreenshotLinks> ScreenshotLinks { get; set; } = default!;
-		public List<Developers> Developers { get; set; } = default!;
+		public List<VideoLink> VideoLinks { get; set; } = default!;
+		public List<ScreenshotLink> ScreenshotLinks { get; set; } = default!;
+		public List<Developer> Developers { get; set; } = default!;
 		public DataType ReleaseDate { get; set; } = default!;
 		public decimal? AverageRate { get; set; }
 		public int RateCount { get; set; } = 0;
 		public List<Genre> Genres { get; set; } = default!;
 		public List<User> Recomendation { get; set; } = default!;
 	}
-	public class VideoLinks
+	public class VideoLink
 	{
 		public int Id { get; set; }
 		public string Link { get; set; } = default!;	
 	}
-	public class ScreenshotLinks
+	public class ScreenshotLink
 	{
 		public int Id { get; set; }
 		public string Link { get; set; } = default!;	
